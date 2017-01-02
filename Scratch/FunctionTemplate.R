657d3c7b1f24fae748e0190bc5bfcd5c12146b7d
Collin Erickson <collinberickson@gmail.com>
2017-01-01 18:03:48 -0600

    Added Scratch and function template

---
+++ Scratch/FunctionTemplate.R
@@ -0,0 +1,24 @@
+#' name123: name123 function
+#' ? dimensional function.
+#' @export
+#' @rdname test_func_apply
+#' @examples
+#' name123(runif(2))
+#' name123(matrix(runif(2*20),ncol=2))
+name123 <- function(x, scale_it=T, scale_low = 0, scale_high = 1, noise=0, ...) {
+  test_func_apply(func=TF_name123, x=x, scale_it=scale_it, scale_low = scale_low, scale_high = scale_high, noise=noise, ...)
+}
+
+#' TF_name123: name123 function for evaluating a single point.
+#'
+#' @param x Input vector at which to evaluate.
+#'
+#' @return Function output evaluated at x.
+#' @export
+#'
+#' @examples
+#' TF_name123(rep(0,2))
+#' TF_name123(rep(1,2))
+TF_name123 <- function(x) {
+
+}
